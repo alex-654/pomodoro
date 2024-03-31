66708734f05e4e74dafb3fe6e21060a41be95fdc
Alexey <vianosenko@gmail.com>
2024-03-31 17:46:35 +0300

    update tests

--- pomodoro_test.go
+++ pomodoro_test.go
@@ -1,70 +1,88 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestPomodoroSuccess(t *testing.T) {
-	tm := &testMessenger{responses: []response{{
+func TestPomodoro(t *testing.T) {
+	pomodoroData := []struct {
+		m    spyMessenger
+		want int
+	}{
+		{
+			spyMessenger{responses: success()},
+			7},
+		{
+			spyMessenger{responses: off()},
+			1,
+		},
+		{
+			spyMessenger{responses: reset()},
+			2,
+		},
+	}
+
+	for _, test := range pomodoroData {
+		p := pomodoro{
+			conf:      conf(),
+			messenger: &test.m,
+		}
+		p.run()
+
+		if test.m.sendCount != test.want {
+			t.Errorf("got %d, expected %d", test.m.sendCount, test.want)
+		}
+	}
+
+}
+
+func success() []response {
+	return []response{{
 		off:      false,
 		reset:    false,
 		nextLoop: 1,
-	}}}
-	p := pomodoro{
-		conf:      conf(),
-		messenger: tm,
-	}
-	p.run()
+	}}
+}
 
-	expected := p.conf.maxLoop*2 + 1
-	if tm.sendCount != expected {
-		t.Errorf("got %d, expected %d", tm.sendCount, expected)
+func off() []response {
+	return []response{
+		{
+			off:   true,
+			reset: false,
+		},
 	}
 }
 
-func TestPomodoroOff(t *testing.T) {
-	tm := &testMessenger{responses: []response{{
-		off:   true,
-		reset: false,
-	}}}
-	p := pomodoro{
-		conf:      conf(),
-		messenger: tm,
-	}
-	p.run()
-
-	expected := 1
-	if tm.sendCount != expected {
-		t.Errorf("got %d, expected %d", tm.sendCount, expected)
+func reset() []response {
+	return []response{
+		{
+			off:   true,
+			reset: false,
+		},
+		{
+			off:   false,
+			reset: true,
+		},
 	}
 }
-
-//func TestPomodoroReset(t *testing.T) {
-//	tm := &testMessenger{responses: []response{{
-//		off:   false,
-//		reset: true,
-//	}}}
-//	p := pomodoro{
-//		conf:      conf(),
-//		messenger: tm,
-//	}
-//	p.run()
-//
-//	expected := 1
-//	if tm.sendCount != expected {
-//		t.Errorf("got %d, expected %d", tm.sendCount, expected)
-//	}
-//}
 
 func conf() config {
 	return config{1, 1, 3}
 }
 
-type testMessenger struct {
+type spyMessenger struct {
 	sendCount int
 	responses []response
 }
 
-func (tm *testMessenger) send(state string, loopCount int, conf config) response {
-	tm.sendCount++
+func (m *spyMessenger) send(state string, loopCount int, conf config) response {
+	m.sendCount++
 
-	return tm.responses[0]
+	length := len(m.responses)
+	res := m.responses[length-1]
+	if length > 1 {
+		m.responses = m.responses[:length-1]
+	}
+
+	return res
 }
